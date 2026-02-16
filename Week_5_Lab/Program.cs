9aa7ffd7f15819beb0da826acb109ea7c430c249
RyanBarry-STUDENT <RyanBarry-STUDENT@S-7L1H504-SL20>
2026-02-16 13:34:12 +0000

    Add project files.

---
+++ Week_5_Lab/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Week_5_Lab
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
