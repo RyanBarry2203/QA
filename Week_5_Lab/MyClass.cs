4f2fcd61eb13c690d39e9faede9bfa339c4a1631
Ryan Barry <barryryan12345@gmail.com>
2026-02-16 14:22:54 +0000

    on part 3

---
+++ Week_5_Lab/MyClass.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using log4net;
+
+namespace Week_5_Lab
+{
+    public class MyClass
+    {
+        private static readonly ILog log = LogManager.GetLogger(typeof(MyClass));
+        public void DoSomething()
+        {
+            log.Debug("This is a debug message.");
+            log.Info("This is an info message");
+            log.Warn("This is a warning message");
+            log.Error("This is an error message");
+            log.Fatal("This is a fatal message");
+
+        }
+        public void ShowException(string message)
+        {
+            log.Error(message);
+        }
+    }
+}
--- Week_5_Lab/Program.cs
+++ Week_5_Lab/Program.cs
@@ -1,10 +1,85 @@
-﻿namespace Week_5_Lab
+﻿using System;
+using System.Diagnostics;
+using System.Text;
+using log4net;
+
+namespace Week_5_Lab
 {
-    internal class Program
+    public class Program
     {
-        static void Main(string[] args)
+        //static void Main(string[] args)
+        //{
+        //    log4net.Config.XmlConfigurator.Configure(new System.IO.FileInfo("log4net.config"));
+
+        //    MyClass myClass = new MyClass();
+        //    myClass.DoSomething();
+
+        //    try
+        //    {
+        //        Console.Write("Enter num 1: ");
+        //        int num1 = int.Parse(Console.ReadLine());
+        //        Console.Write("Enter num 2: ");
+        //        int num2 = int.Parse(Console.ReadLine());
+
+        //        int result = num1 / num2;
+        //        Console.WriteLine($"Result: {result}");
+        //    }
+        //    catch (DivideByZeroException ex)
+        //    {
+        //        log4net.LogManager.GetLogger(typeof(Program)).Error("An error occurred while dividing by zero.", ex);
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        log4net.LogManager.GetLogger(typeof(Program)).Error("An unexpected error occurred.", ex);
+        //    }
+        //}
+        public Stopwatch sw = Stopwatch.StartNew();
+        public static int slowTime;
+
+        static void Main()
         {
-            Console.WriteLine("Hello, World!");
+            log4net.Config.XmlConfigurator.Configure(new System.IO.FileInfo("log4net.config"));
+
+            int iterations = 100_000;
+
+            Stopwatch sw = Stopwatch.StartNew();
+            string slowResult = SlowConcat(iterations);
+            sw.Stop();
+
+            MyClass myClass1 = new MyClass();
+            myClass1.ShowException($"SlowConcat Time: {sw.ElapsedMilliseconds} ms");
+
+            slowTime = (int)sw.ElapsedMilliseconds;
+
+            sw.Restart();
+            string fastResult = FastConcat(iterations);
+            sw.Stop();
+            myClass1.ShowException($"FastConcat Time: {sw.ElapsedMilliseconds} ms");
+        }
+
+        // Inefficient method using string concatenation inside a loop
+        static string SlowConcat(int count)
+        {
+            string result = "";
+            for (int i = 0; i < count; i++)
+            {
+                result += i; // Creates a new string object each time
+            }
+            return result;
+        }
+
+        // Efficient method using StringBuilder
+        static string FastConcat(int count)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = 0; i < count; i++)
+            {
+                sb.Append(i); // Modifies the existing buffer, avoiding new allocations
+            }
+            return sb.ToString();
         }
     }
-}
+    }
+
+
